ad8520f87a655e80107f37f93f60851f5de9c53e
Toma <chou@TomadeMacBook-Air.local>
2026-05-29 15:31:09 +0800

    fix: 修复App.jsx路由，添加react-router-dom，支持双人对战

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import love8Theme from './theme/love8Theme';
+import Lobby from './components/Lobby';
 import GameBoard from './components/GameBoard';
 
 function App() {
   return (
     <ThemeProvider theme={love8Theme}>
-      <GameBoard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Lobby />} />
+          <Route path="/game" element={<GameBoard />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
